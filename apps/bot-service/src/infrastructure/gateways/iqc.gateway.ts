4b363a0c0146f1ef2414f2af3bc0c8010325f2b9
Justdhif <noob1234five@gmail.com>
2026-08-02 16:16:09 +0700

    debug: catch and print generateIQC core engine errors inside iqc gateway

--- apps/bot-service/src/infrastructure/gateways/iqc.gateway.ts
+++ apps/bot-service/src/infrastructure/gateways/iqc.gateway.ts
@@ -2,10 +2,15 @@ import { generateIQC } from 'iqc-canvas';
 
 
 export async function generateIqcScreenshot(text: string, time: string): Promise<Buffer> {
-  return await generateIQC(text, time, {
-    baterai: [true, '100'],
-    operator: true,
-    timebar: true,
-    wifi: true,
-  });
+  try {
+    return await generateIQC(text, time, {
+      baterai: [true, '100'],
+      operator: true,
+      timebar: true,
+      wifi: true,
+    });
+  } catch (error: any) {
+    console.error('[GENERATE IQC FAIL]', error);
+    throw error;
+  }
 }
